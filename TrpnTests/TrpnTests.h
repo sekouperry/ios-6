bd7fc822aaa9dd4c4399f7c526a538c5289a3070
Keith Norman <keithnorm@gmail.com>
2013-03-21 16:12:05 -0700

    Initial Commit

---
+++ TrpnTests/TrpnTests.h
@@ -0,0 +1,13 @@
+//
+//  TrpnTests.h
+//  TrpnTests
+//
+//  Created by Keith Norman on 3/21/13.
+//  Copyright (c) 2013 Trpn. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TrpnTests : SenTestCase
+
+@end
---
+++ TrpnTests/TrpnTests.m
@@ -0,0 +1,32 @@
+//
+//  TrpnTests.m
+//  TrpnTests
+//
+//  Created by Keith Norman on 3/21/13.
+//  Copyright (c) 2013 Trpn. All rights reserved.
+//
+
+#import "TrpnTests.h"
+
+@implementation TrpnTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TrpnTests");
+}
+
+@end
